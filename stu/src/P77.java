09543bc32e8703605faf7eeb8595dca1b70f02cf
Aio392 <Aio392@DESKTOP-V3N6ENS>
2017-10-26 13:15:59 +0900

    test

---
+++ stu/src/P77.java
@@ -0,0 +1,17 @@
+
+public class P77 {
+
+	public static void main(String[] args) {
+int yuny = 2100;
+
+
+
+System.out.println(
+( yuny%400 == 0 ) ||  (yuny%4 == 0)  &&   (yuny%100  != 0)
+);
+
+
+
+	}
+
+}
